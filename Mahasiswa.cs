b80de2ac65f7ef73e9b3533482ce62215acf1b27
DI-Farhan <pegasus998877@gmail.com>
2019-03-19 14:53:55 +0700

    Project Class

---
+++ Mahasiswa.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LatihanClassProject
+{
+    public class Mahasiswa
+    {
+        public string Nim { get; set; }
+        public string Nama { get; set; }
+        public float Ipk { get; set; }
+
+        public void Registrasi()
+        {
+            Console.WriteLine("Nama: {0}", Nama);
+            Console.WriteLine("NIM: {0}", Nim);
+            Console.WriteLine("IPK: {0}", Ipk);
+            Console.WriteLine("Telah melakukan registrasi");
+        }
+        public void IsiKrs()
+        {
+            Console.WriteLine("{0} Sedang melakukan proses KRS\n", Nama);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace LatihanClassProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mahasiswa saya = new Mahasiswa();
+            Mahasiswa kamu = new Mahasiswa();
+            saya.Nim = "12345";
+            saya.Nama = "Farhan";
+            saya.Ipk = 3.95f;
+
+            kamu.Nim = "12345";
+            kamu.Nama = "Sukijah";
+            kamu.Ipk = 3.50f;
+
+            saya.Registrasi();
+            saya.IsiKrs();
+
+            kamu.Registrasi();
+            kamu.IsiKrs();
+
+            Console.ReadKey();
+        }
+    }
+}
